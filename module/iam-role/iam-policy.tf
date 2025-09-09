aec37c47c5a18160016128172216c43b18ce6f67
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-09-09 10:13:09 +0530

    Adding modules files

---
+++ module/iam-role/iam-policy.tf
@@ -0,0 +1,43 @@
+#ECS Service policy
+data "aws_iam_policy_document" "tasks-service-assume-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "tasks-service-role-attachment" {
+  role       = aws_iam_role.tasks-service-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+#AWS build policy
+data "aws_iam_policy_document" "tf-cicd-build-policies" {
+    statement{
+        sid = ""
+        actions = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*","iam:*","codecommit:GitPull"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+
+resource "aws_iam_policy" "tf-cicd-build-policy" {
+    name = "tf-cicd-build-policy"
+    path = "/"
+    description = "Codebuild policy"
+    policy = data.aws_iam_policy_document.tf-cicd-build-policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment1" {
+    policy_arn  = aws_iam_policy.tf-cicd-build-policy.arn
+    role        = aws_iam_role.tf-codebuild-role.id
+}
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment2" {
+    policy_arn  = "arn:aws:iam::aws:policy/PowerUserAccess"
+    role        = aws_iam_role.tf-codebuild-role.id
+}
--- module/iam-role/iam-role.tf
+++ module/iam-role/iam-role.tf
@@ -1,21 +1,32 @@
+#ECS Service role
 resource "aws_iam_role" "tasks-service-role" {
   name               = var.tasks-service-role
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.tasks-service-assume-policy.json
 }
 
-data "aws_iam_policy_document" "tasks-service-assume-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
+
+#AWS build role
+resource "aws_iam_role" "tf-codebuild-role" {
+  name = "tf-codebuild-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
-  }
+  ]
+}
+EOF
+
 }
 
-resource "aws_iam_role_policy_attachment" "tasks-service-role-attachment" {
-  role       = aws_iam_role.tasks-service-role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+
