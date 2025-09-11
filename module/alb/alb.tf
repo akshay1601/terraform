7b00cb9454c4a96808db4f3cd2adfd208b29058d
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-09-11 09:35:15 +0530

    Adding modules files

--- module/alb/alb.tf
+++ module/alb/alb.tf
@@ -39,16 +39,16 @@ resource "aws_lb_target_group" "ip-service-8080" {
 
 
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.service_alb.id
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.service_alb.id
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    # target_group_arn = aws_lb_target_group.ip-service.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#      target_group_arn = aws_lb_target_group.ip-service.arn
+#   }
+# }
 resource "aws_lb_listener" "front_end-8080" {
   load_balancer_arn = aws_lb.service_alb.id
   port              = 8080
