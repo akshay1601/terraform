0af234abc65216dbbc36c70f29ff317ed78a8d87
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-09-11 19:39:59 +0530

    Adding modules files

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-module "ec2" {
-    source = "./module/ec2"
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    instance_count  = var.instance_count
-    instance_name = var.instance_name
-    volume_size = var.volume_size
-    volume_type = var.volume_type
+# module "ec2" {
+#     source = "./module/ec2"
+#     ami = var.ami
+#     instance_type = var.instance_type
+#     key_name = var.key_name
+#     instance_count  = var.instance_count
+#     instance_name = var.instance_name
+#     volume_size = var.volume_size
+#     volume_type = var.volume_type
 
-    }
+#     }
 
 module "sg" {
   source = "./module/sg"
