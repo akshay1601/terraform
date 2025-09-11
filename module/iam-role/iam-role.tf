023504508a37f8a68ed445ebc28c942912bdecc0
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-09-11 20:00:21 +0530

    Adding modules files

--- module/iam-role/iam-role.tf
+++ module/iam-role/iam-role.tf
@@ -10,21 +10,22 @@ resource "aws_iam_role" "tasks-service-role" {
 #AWS build role
 resource "aws_iam_role" "tf-codebuild-role" {
   name = "tf-codebuild-role"
+  assume_role_policy = data.aws_iam_policy_document.tf-cicd-build-policies.json
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "codebuild.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
 
 }
--- module/s3/s3.tf
+++ module/s3/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "aws_s3_bucket_build" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "aws_s3_bucket_acl_build" {
-  bucket = aws_s3_bucket.aws_s3_bucket_build.id
-  acl    = var.acl
-}
+# resource "aws_s3_bucket_acl" "aws_s3_bucket_acl_build" {
+#   bucket = aws_s3_bucket.aws_s3_bucket_build.id
+#   acl    = var.acl
+# }
