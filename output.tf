42263e8a50a2e1e7575fd8ad64332c97e10a8136
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-09-13 22:28:02 +0530

    Adding modules files

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "secret_value" {
-  value     =  module.build.secret_value
+  value     =  module.build.github_secret
   sensitive = true # Mark as sensitive to prevent logging in plain text
 
 }
