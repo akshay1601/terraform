f3f3362969c47e2c6d6194bbebe83d28a9b31372
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-09-13 22:55:59 +0530

    Adding modules files

--- module/build/output.tf
+++ module/build/output.tf
@@ -3,6 +3,6 @@ output "secret_value" {
   sensitive = true # Mark as sensitive to prevent logging in plain text
 }
 output "github_secret" {
-      value = jsondecode(data.aws_secretsmanager_secret_version.codebuild_token_secret_version.secret_string).Token.value
+      value = jsondecode(data.aws_secretsmanager_secret_version.codebuild_token_secret_version.secret_string).Token
       sensitive = true
     }
